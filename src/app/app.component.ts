75095ceafd51232387b366a95f13e6f77a900732
Frederick <antoniobalderramamendoza@gmail.com>
2024-10-08 00:00:52 -0400

    Tu carrera component contiene las rutas de cada carrera, falta responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-      <router-outlet></router-outlet>
-    </section>
-  </main>
-`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
